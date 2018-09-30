1f64f89ec7d2c525fa017b6bd4cb3629fb53a002
Dumindu Madunuwan <dumindu.madunuwan@gmail.com>
2018-09-30 08:55:25 +0700

    [v0.3]

--- app.js
+++ app.js
@@ -1,38 +1,4 @@
 const themes = {
-  "light": {
-    colors: {
-      accentcolor: "#e6e8e3",
-      tab_background_separator: "#e6e8e3",
-      toolbar: "#fcfbf9",
-      textcolor: "#8e8e8c",
-      tab_line: "#fcfbf9",
-      tab_text: "#545351",
-
-      toolbar_top_separator: "#e6e8e3",
-      toolbar_vertical_separator: "#e6e8e3",
-      toolbar_bottom_separator: "#e6e8e3",
-
-      toolbar_field: "#fcfbf9",
-      toolbar_field_text: "#8e8e8c",
-      toolbar_field_border: "#fcfbf9",
-      toolbar_field_separator: "#e6e8e3",
-
-      toolbar_field_focus: "#fcfbf9",
-      toolbar_field_text_focus: "#8e8e8c",
-      toolbar_field_border_focus: "#e6e8e3",
-
-      button_background_active: "#e6e8e3",
-      button_background_hover: "#e6e8e3",
-
-      popup: "#fcfbf9",
-      popup_text: "#545351",
-      popup_border: "#c5c4c2",
-      popup_highlight: "#e6e8e3",
-      popup_highlight_text: "#545351",
-
-      tab_loading: "#e6e8e3",
-    }
-  },
   "dark": {
     colors: {
       accentcolor: "#424242",
@@ -40,6 +6,7 @@ const themes = {
       toolbar: "#363636",
       textcolor: "#8f8f8f",
       tab_line: "#363636",
+      tab_loading: "#424242",
 
       toolbar_top_separator: "#333333",
       toolbar_vertical_separator: "#474747",
@@ -64,14 +31,57 @@ const themes = {
       popup_highlight: "#424242",
       popup_highlight_text: "#8f8f8f",
 
-      tab_loading: "#424242",
+      sidebar: "#363636",
+      sidebar_text: "#8f8f8f",
+      sidebar_border: "#333333",
+      sidebar_highlight: "#424242",
+      sidebar_highlight_text: "#8f8f8f",
+    }
+  },
+  "light": {
+    colors: {
+      accentcolor: "#e6e8e3",
+      tab_background_separator: "#e6e8e3",
+      toolbar: "#fcfbf9",
+      textcolor: "#8e8e8c",
+      tab_line: "#fcfbf9",
+      tab_text: "#545351",
+      tab_loading: "#e6e8e3",
+
+      toolbar_top_separator: "#e6e8e3",
+      toolbar_vertical_separator: "#e6e8e3",
+      toolbar_bottom_separator: "#e6e8e3",
+
+      toolbar_field: "#fcfbf9",
+      toolbar_field_text: "#8e8e8c",
+      toolbar_field_border: "#fcfbf9",
+      toolbar_field_separator: "#e6e8e3",
+
+      toolbar_field_focus: "#fcfbf9",
+      toolbar_field_text_focus: "#8e8e8c",
+      toolbar_field_border_focus: "#e6e8e3",
+
+      button_background_active: "#e6e8e3",
+      button_background_hover: "#e6e8e3",
+
+      popup: "#fcfbf9",
+      popup_text: "#545351",
+      popup_border: "#c5c4c2",
+      popup_highlight: "#e6e8e3",
+      popup_highlight_text: "#545351",
+
+      sidebar: "#fcfbf9",
+      sidebar_text: "#545351",
+      sidebar_border: "#c5c4c2",
+      sidebar_highlight: "#e6e8e3",
+      sidebar_highlight_text: "#545351",
     }
   }
 };
 
 function updateTheme(theme) {
   if (!theme) {
-    theme = "light";
+    theme = "dark";
   }
   browser.storage.local.set({"theme": theme});
   browser.theme.update(themes[theme]);
@@ -79,7 +89,7 @@ function updateTheme(theme) {
     path: "icons/" + theme + "-mode.svg"
   });
   browser.browserAction.setTitle({
-    title: theme == "light" ? 'Enable dark theme 🌑' : 'Enable light theme ☀️',
+    title: theme == "dark" ? 'Enable light theme ☀️' : 'Enable dark theme 🌑',
   });
 }
 
